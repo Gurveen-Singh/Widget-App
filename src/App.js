cc9f2b037047dd5e83d2e8e0791ab5c2856feb2f
Gurveen-Singhm <gurveensingh45@gmail.com>
2021-08-12 00:20:23 +0530

    Accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a frontend JS library.",
+  },
+  {
+    title: "Why React?",
+    content: "Engineers love React.",
+  },
+  {
+    title: "Why React?",
+    content: "Engineers love React.",
+  },
+];
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 }
 
 export default App;
